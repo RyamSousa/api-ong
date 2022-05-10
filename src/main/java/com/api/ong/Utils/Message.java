685a787d0d87c4217fa114ebadf3802fdadcdaf4
Ryam Sousa <ryam.sousa123@gmail.com>
2022-05-10 15:31:09 -0300

    :hammer: refact utils

--- src/main/java/com/api/ong/Utils/Message.java
+++ src/main/java/com/api/ong/Utils/Message.java
@@ -2,8 +2,9 @@ package com.api.ong.Utils;
 
 public class Message {
 
-    public static final String USER_NOT_FOUND = "User not found";
-    public static final String NO_RECORDS_FOUND = "No records found";
-    public static final String USER_ALREADY_EXISTS = "User already exists";
+    public static final String RESOURCE_NOT_FOUND = "Resource not found";
     public static final String ID_CANT_BE_NULL = "Id can't be null";
+    public static final String NO_RECORDS_FOUND = "No records found";
+    public static final String RESOURCE_ALREADY_EXISTS = "Resource already exists";
+    public static final String USER_OR_ONG_NOT_FOUND = "User or Ong not found";
 }
