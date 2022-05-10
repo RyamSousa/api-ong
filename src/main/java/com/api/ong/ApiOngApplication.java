f9cdeb5cba5b17ceb7f067b7507cb1a9f5f99c9a
RyamSousa <ryam.sousa123@gmail.com>
2022-05-09 22:08:56 -0300

    replace project

---
+++ src/main/java/com/api/ong/ApiOngApplication.java
@@ -0,0 +1,13 @@
+package com.api.ong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/ong/ApiOngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
