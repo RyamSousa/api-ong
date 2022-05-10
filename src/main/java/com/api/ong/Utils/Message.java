8d0f92504212a2e6968afbcedc90b78025625870
Ryam Sousa <ryam.sousa123@gmail.com>
2022-05-10 10:06:01 -0300

    :sparkles: added 'util' class for messages

---
+++ src/main/java/com/api/ong/Utils/Message.java
@@ -0,0 +1,9 @@
+package com.api.ong.Utils;
+
+public class Message {
+
+    public static final String USER_NOT_FOUND = "User not found";
+    public static final String NO_RECORDS_FOUND = "No records found";
+    public static final String USER_ALREADY_EXISTS = "User already exists";
+    public static final String ID_CANT_BE_NULL = "Id can't be null";
+}
