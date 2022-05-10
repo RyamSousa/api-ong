67b6f43c096596970eb8875208c89c41a8837a54
Ryam Sousa <ryam.sousa123@gmail.com>
2022-05-10 15:32:10 -0300

    :hammer: refact repository

--- src/main/java/com/api/ong/repository/AnimalRepository.java
+++ src/main/java/com/api/ong/repository/AnimalRepository.java
@@ -3,5 +3,10 @@ package com.api.ong.repository;
 import com.api.ong.model.AnimalModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AnimalRepository extends JpaRepository<AnimalModel, Long> {
+
+    Optional<List<AnimalModel>> findBySpecie(String specie);
 }
