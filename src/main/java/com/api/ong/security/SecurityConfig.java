af799e37b605e03c5ea5fd69a74781d381c34d85
RyamSousa <ryam.sousa123@gmail.com>
2022-05-11 09:39:46 -0300

    :hammer: :bug: refact 'cors' filter (2)

--- src/main/java/com/api/ong/security/SecurityConfig.java
+++ src/main/java/com/api/ong/security/SecurityConfig.java
@@ -1,15 +1,25 @@
 package com.api.ong.security;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-@Configuration
-public class SecurityConfig implements WebMvcConfigurer {
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class SecurityConfig extends OncePerRequestFilter {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:8080");
-        WebMvcConfigurer.super.addCorsMappings(registry);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+            response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+            response.addHeader("Access-Control-Max-Age", "1");
+        }
+        filterChain.doFilter(request, response);
     }
 }
