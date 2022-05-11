7d8689b8f4270553abc59c79e7cf4cd79490ed5d
RyamSousa <ryam.sousa123@gmail.com>
2022-05-11 09:28:55 -0300

    :hammer: :bug: refact 'cors' filter

--- src/main/java/com/api/ong/security/SecurityConfig.java
+++ src/main/java/com/api/ong/security/SecurityConfig.java
@@ -1,18 +1,15 @@
 package com.api.ong.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig implements WebMvcConfigurer {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
-                .and().authorizeRequests().anyRequest().permitAll()
-                .and().csrf().disable();
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+        WebMvcConfigurer.super.addCorsMappings(registry);
     }
 }
